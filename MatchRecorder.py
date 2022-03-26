1326c07a520310bb926a604d00079933de2c3393
Ofek Harel <42820258+OfekHarel@users.noreply.github.com>
2022-03-26 12:39:58 +0300

    Update MatchRecorder.py

--- MatchRecorder.py
+++ MatchRecorder.py
@@ -9,12 +9,13 @@ class MatchRecorder:
     def __init__(self, FPS, output):
         self.SCREEN_SIZE = tuple(pyautogui.size())
         self.FPS = FPS
-        self.REC_SEC = 150
         self.OUT_PATH = output
         self.MATCH_TYPE_MAP = {0: 'NA', 1: 'PM', 2: 'QM', 3: 'EM'}
         self.vid_out = None
         self.fourcc = cv2.VideoWriter_fourcc(*'mp4v')
         self.table = NetworkTables.getTable('FMSInfo')
+
+        self.was_init = False
 
     
     def _get_match_discription(self):
@@ -28,14 +29,19 @@ class MatchRecorder:
     def _get_vid_out(self):
         return cv2.VideoWriter(f"{os.path.join(self.OUT_PATH, self._get_match_discription())}.mp4", self.fourcc, self.FPS, (self.SCREEN_SIZE))
 
-
-    def start(self):
-        print("starting...")
-        self.vid_out = self._get_vid_out()
-        for _ in range(int(self.REC_SEC * self.FPS)):
-            img = pyautogui.screenshot()
-            frame = np.array(img)
-            frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-            self.vid_out.write(frame)
-        self.vid_out.release()
+    def next_frame(self):
+        if not self.was_init:
+            print("starting...")
+            self.vid_out = self._get_vid_out()
+            self.was_init = True
+            
+        img = pyautogui.screenshot()
+        frame = np.array(img)
+        frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        self.vid_out.write(frame)
+    
+    def stop(self):
         print("closing...")
+        self.vid_out.release()
+        self.was_init = False
+
