d3df506f5be1ef12b1e7ed1387d4e269ddbe8024
Ofek Harel <42820258+OfekHarel@users.noreply.github.com>
2022-03-26 12:40:30 +0300

    Update BackgroundPeriodicRecorder.py

--- BackgroundPeriodicRecorder.py
+++ BackgroundPeriodicRecorder.py
@@ -1,12 +1,15 @@
 from networktables import NetworkTables
 from MatchRecorder import MatchRecorder
 from NT_helper import nt_init
+import os
+
 
 class PeriodicRecorder:
 
     def __init__(self, enable_entry_path, fps=15, output='', simulation=False):
         self.enable_entry_path = enable_entry_path
         self.recorder = MatchRecorder(fps, output)
+        self.started = False
 
         print("waiting for connections...")
         ip = '10.33.39.2' if not simulation else 'localhost'
@@ -14,20 +17,23 @@ class PeriodicRecorder:
             self.enable_entry_path = "/recorder/start"
             NetworkTables.startClient(ip)
         nt_init(ip)
+
     
     def is_recording(self):
         return NetworkTables.getEntry(self.enable_entry_path).getBoolean(False)
-    
-    def stop(self):
-        NetworkTables.getEntry(self.enable_entry_path).setBoolean(False)
 
     def start(self):
         while True:
             if self.is_recording():
-                self.recorder.start()
-                self.stop()
+                self.started = True
+                self.recorder.next_frame()
+
+            elif self.started:
+                self.started = False
+                self.recorder.stop()
+
 
 if __name__ == '__main__':
-    pr = PeriodicRecorder("/Smartdashboard/DS_recording/isStart", fps=15, output=f'C:\Users\Laptop7\Videos\Captures', simulation=False)
+    pr = PeriodicRecorder("/SmartDashboard/DS_recording/isStarted", fps=15, output=os.path.join(os.environ['USERPROFILE'], r'Videos\Captures'), simulation=False)
     pr.start()
             
