3561853e3a17af26e03a12517bc6944b770765ec
Ofek Harel <42820258+OfekHarel@users.noreply.github.com>
2022-03-26 13:58:49 +0300

    Update MatchRecorder.py

--- MatchRecorder.py
+++ MatchRecorder.py
@@ -1,8 +1,10 @@
+from math import ceil
 import cv2
 from networktables import NetworkTables
 import numpy as np
 import pyautogui
 import os
+import time
 
 class MatchRecorder:
 
@@ -12,12 +14,12 @@ class MatchRecorder:
         self.OUT_PATH = output
         self.MATCH_TYPE_MAP = {0: 'NA', 1: 'PM', 2: 'QM', 3: 'EM'}
         self.vid_out = None
+        self.was_init = False
+        self.init_time = None
         self.fourcc = cv2.VideoWriter_fourcc(*'mp4v')
         self.table = NetworkTables.getTable('FMSInfo')
 
-        self.was_init = False
 
-    
     def _get_match_discription(self):
         match_type = self.table.getNumber('MatchType', 0)
         match_number = self.table.getNumber('MatchNumber', 0)
@@ -29,19 +31,28 @@ class MatchRecorder:
     def _get_vid_out(self):
         return cv2.VideoWriter(f"{os.path.join(self.OUT_PATH, self._get_match_discription())}.mp4", self.fourcc, self.FPS, (self.SCREEN_SIZE))
 
+
+    def get_match_time(self) -> str:
+        if self.was_init:
+            return str(int(time.time() - self.init_time))
+        else:
+            return str(0)
+
     def next_frame(self):
+        img = pyautogui.screenshot()
+        frame = np.array(img)
+        frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        frame = cv2.putText(frame, self.get_match_time().zfill(3), (self.SCREEN_SIZE[0] - 200, self.SCREEN_SIZE[1] - 50), cv2.FONT_HERSHEY_SIMPLEX, 3, (255, 255, 255), 4)
+
         if not self.was_init:
             print("starting...")
             self.vid_out = self._get_vid_out()
             self.was_init = True
-            
-        img = pyautogui.screenshot()
-        frame = np.array(img)
-        frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+            self.init_time = time.time()
+
         self.vid_out.write(frame)
     
     def stop(self):
         print("closing...")
         self.vid_out.release()
         self.was_init = False
-
